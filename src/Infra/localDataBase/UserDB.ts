6a5503164ed88bb4e5894aa15e61c6a07b66677c
engnandogabriel <eng.nandogabriel@gmail.com>
2023-08-21 14:08:14 -0300

    fix: added CorrectionValues method and two users in local storage

--- src/Infra/localDataBase/UserDB.ts
+++ src/Infra/localDataBase/UserDB.ts
@@ -1,10 +1,37 @@
 import { UserRepository } from "../../data/Contracts/UserRepository";
 import User from "../../domain/entities/User";
 
+const user1: User = new User({
+  firstName: "Nando Gabriel",
+  lastName: "Machado Bezerra",
+  document: "62803120380",
+  email: "nando@gmail.com",
+  password: "123456",
+  balance: 1000,
+  typeUser: "common",
+});
+
+const user2: User = new User({
+  firstName: "Marcos",
+  lastName: "Leonardo",
+  document: "03055916301",
+  email: "marcosg@gmail.com",
+  password: "123456",
+  balance: 500,
+  typeUser: "merchant",
+});
+
 export class UserDb implements UserRepository {
   data: User[];
   constructor() {
-    this.data = [];
+    this.data = [user1, user2];
+  }
+
+  async findPayer(id: string): Promise<void | User> {
+    return this.data.find((user) => user.id === id);
+  }
+  async findPayee(id: string): Promise<void | User> {
+    return this.data.find((user) => user.id === id);
   }
 
   async findByEmail(email: string): Promise<void | User> {
@@ -18,5 +45,16 @@ export class UserDb implements UserRepository {
   }
   async getUsers(): Promise<User[]> {
     return this.data;
+  }
+
+  async correctionValues(
+    payer: User,
+    payee: User,
+    value: number
+  ): Promise<void> {
+    this.data.forEach((user) => {
+      if (user.id === payee.id) user.balance = payee.balance + Number(value);
+      if (user.id === payer.id) user.balance = payer.balance - value;
+    });
   }
 }
