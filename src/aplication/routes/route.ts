14d3a9a7e3aa5a7587dbaa9a2a3d43228cb2c62f
engnandogabriel <eng.nandogabriel@gmail.com>
2023-08-21 14:08:29 -0300

    feat: index and routes

---
+++ src/aplication/routes/route.ts
@@ -0,0 +1,42 @@
+import { Router } from "express";
+import { UserDb } from "../../Infra/localDataBase/UserDB";
+import { CreateUserController } from "../controllers/CreateUserController";
+import CreateUser from "../../data/services/CreateUser/CreateUser";
+import { GetUsers } from "../../data/services/GetUser/GetUser";
+import { GetUsersController } from "../controllers/GetUserController";
+import TransactionDB from "../../Infra/localDataBase/TransactionDB";
+import CreateTransaction from "../../data/services/CreateTransaction/CreateTransaction";
+import CreateTransactionController from "../controllers/CreateTransacrionController";
+import GetTransaction from "../../data/services/GetTransaction/GetTransaction";
+import GetTransactionController from "../controllers/GetTransactionController";
+
+const router = Router();
+const userDb = new UserDb();
+const transactionDb = new TransactionDB();
+
+router.post("/user", async (req, res) => {
+  const createUser = new CreateUser(userDb);
+  const createUserController = new CreateUserController(createUser);
+  return await createUserController.handle(req, res);
+});
+
+router.get("/user", async (req, res) => {
+  const getUser = new GetUsers(userDb);
+  const getUsersController = new GetUsersController(getUser);
+  return await getUsersController.handle(req, res);
+});
+
+router.post("/transaction", async (req, res) => {
+  const createTransaction = new CreateTransaction(transactionDb, userDb);
+  const createTranactionController = new CreateTransactionController(
+    createTransaction
+  );
+  return await createTranactionController.handle(req, res);
+});
+
+router.get("/transaction", async (req, res) => {
+  const getTransacion = new GetTransaction(transactionDb);
+  const getTransactionController = new GetTransactionController(getTransacion);
+  return await getTransactionController.handle(req, res);
+});
+export default router;
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import express from "express";
-import routerUser from "./aplication/routes/user.routes";
+import router from "./aplication/routes/route";
 const app = express();
 app.use(express.json());
 
-app.use("/user", routerUser);
+app.use("/", router);
 
 app.listen(8080, () => console.log("Serve running in port 8080"));
