138868d81ec3d6c164dca958499bd3738f10cddf
engnandogabriel <eng.nandogabriel@gmail.com>
2023-08-21 14:05:44 -0300

    fix: added methods in to UserRepository

--- src/data/Contracts/UserRepository.ts
+++ src/data/Contracts/UserRepository.ts
@@ -3,6 +3,9 @@ import User from "../../domain/entities/User";
 export interface UserRepository {
   findByEmail(email: string): Promise<User | void>;
   findByDocument(document: string): Promise<User | void>;
+  findPayer(id: string): Promise<User | void>;
+  findPayee(id: string): Promise<User | void>;
   save(user: User): Promise<void>;
   getUsers(): Promise<User[]>;
+  correctionValues(payer: User, payee: User, value: number): Promise<void>;
 }
